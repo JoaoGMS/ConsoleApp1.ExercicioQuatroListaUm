d528ea18931012050f2db0db8adba808b1a3b60b
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-18 17:00:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioQuatroListaUm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioQuatroListaUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de Celcius para Fahrenheit ");
+            while (true)
+            {
+                
+                double celsius, fahrenheit;
+
+                Console.Write("Digite a temperatura em celsius: ");
+                celsius = Convert.ToDouble(Console.ReadLine());
+
+                fahrenheit = celsius * 1.8 + 32;
+
+                Console.WriteLine("A conversão para fahrenheit resulta em {0}", Math.Round(fahrenheit, 1));
+
+                Console.ReadKey();
+            }while(true);
+        }
+    }
+}
